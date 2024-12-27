b27bbd3c7014de2376094bbecaec79b703702d5e
Faysal64 <fdriouach10@gmail.com>
2024-12-27 15:12:37 +0100

    Initial commit

---
+++ Kasa/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div>
+            <img src='Logo.png' alt='logo'></img>
+        </div>
+      <div>Acceuil</div>
+      <div>A propos</div>
+    </nav>
+  )
+}
+
+export default Navbar
