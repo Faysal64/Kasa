6d132d91f73443ab1da5656d283040bc6902d6af
Faysal64 <fdriouach10@gmail.com>
2025-01-13 15:41:33 +0100

    commit13/01

--- Kasa/src/components/About.jsx
+++ Kasa/src/components/About.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import Header from './Header'; 
-import Footer from './Footer'; 
-import '../App.css'; 
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import '../About.css';
 
-function About() {
-    return (
-      <div>
-        <Header />
-        <Footer />
+const About = () => {
+  const [visibleIndex, setVisibleIndex] = useState(null);
+
+  const toggleDescription = (index) => {
+    setVisibleIndex(visibleIndex === index ? null : index);
+  };
+
+  const descriptions = [
+    { title: 'Fiabilité', text: 'Nous assurons la fiabilité de nos services en respectant nos engagements.' },
+    { title: 'Respect', text: 'Le respect de la vie privée de nos utilisateurs est une priorité.' },
+    { title: 'Service', text: 'Nous offrons un service client disponible et à l’écoute.' },
+    { title: 'Sécurité', text: 'La sécurité de vos données est notre principale préoccupation.' },
+  ];
+
+  return (
+    <div className="app-container"> {/* Conteneur principal avec Flexbox */}
+      <Header />
+      <div className="sizeImage-container">
+        <img src="/Mountain.png" alt="Mountain" className="sizeImage" />
       </div>
-    );
-  }
+      <div className="description-container1">
+        {descriptions.map((item, index) => (
+          <div className="description-item1" key={index}>
+            <p
+              className="backRed2"
+              onClick={() => toggleDescription(index)}
+            >
+              {item.title}
+              <span className={`icon ${visibleIndex === index ? 'rotate' : ''}`}>
+                <i className="fa-solid fa-chevron-up"></i>
+              </span>
+            </p>
+            {visibleIndex === index && <p className="description-text">{item.text}</p>}
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default About;
--- Kasa/src/components/NotFound.jsx
+++ Kasa/src/components/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import du composant Link
 import Header from './Header';
 import Footer from './Footer';
 import '../NotFound.css';
@@ -10,7 +11,7 @@ const NotFound = () => {
       <div className="flexCentre">
         <div className="erreur">404</div>
         <div className="color">Oups! La page que vous demandez n'existe pas.</div>
-        <div>Retourner sur la page d'accueil</div>
+        <Link to="/" style={{ color: 'black', textDecoration: 'underline' }}>Retourner sur la page d'accueil</Link>
       </div>
       <Footer />
     </>
